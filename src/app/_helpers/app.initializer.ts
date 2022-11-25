6ca0659350b8f53652f81daa14803f2ca47687d9
Brian Macias <maciasaronabrian@gmail.com>
2022-11-25 02:09:19 -0300

    Initial commit

--- src/app/_helpers/app.initializer.ts
+++ src/app/_helpers/app.initializer.ts
@@ -1,19 +1,5 @@
-import { AccountService } from '@app/_services';
+import { AccountService } from '../_services';
 
 export function appInitializer(accountService: AccountService) {
-  return () =>
-    new Promise<void>((resolve) => {
-      // attempt to refresh token on app start up to auto authenticate
-      accountService.refreshToken().subscribe(() => resolve());
-    });
+  return () => accountService.refreshToken().toPromise();
 }
-
-// import { AccountService } from '@app/_services';
-
-// export function appInitializer(accountService: AccountService) {
-//   return () =>
-//     new Promise((resolve) => {
-//       // attempt to refresh token on app start up to auto authenticate
-//       accountService.refreshToken().subscribe().add(resolve);
-//     });
-// }
